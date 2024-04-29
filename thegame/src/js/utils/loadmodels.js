a254b1ca00d1d70bb4d11fe3b68682a4888eaaeb
Krugou <59641229+Krugou@users.noreply.github.com>
2024-04-29 22:06:56 +0300

    updates

--- thegame/src/js/utils/loadmodels.js
+++ thegame/src/js/utils/loadmodels.js
@@ -15,7 +15,7 @@ async function loadModel(loader, modelPath, position, teleportEnabled = false) {
 	loader.load(modelPath, async function (gltf) {
 		const model = gltf.scene;
 		model.position.set(...position);
-		if (teleportEnabled && modelPath.includes('floor')) {
+		if (teleportEnabled) {
 			console.log('teleport');
 			teleportGroup.add(model);
 		} else {
@@ -62,6 +62,63 @@ export function loadmodels(target) {
 				// clonedModel.position.z += 2.75; // adjust the value as needed
 				clonedModel.position.set(0, 0, 0);
 				clonedModel.children[0].name = '7th-floor';
+				console.log(clonedModel);
+				interactionGroup.add(clonedModel);
+
+				await renderer.compileAsync(model, camera, scene);
+			});
+			loader.load('objects/hissi/hissi.gltf', async function (gltf) {
+				const model = gltf.scene;
+
+				model.position.set(0, 0, 0);
+
+				scene.add(model);
+				const clonedModel = model.clone();
+				clonedModel.position.z -= 2.75; // adjust the value as needed
+				scene.add(clonedModel);
+				const anotherClonedModel = clonedModel.clone();
+				anotherClonedModel.position.z -= 2.75; // adjust the value as needed
+				scene.add(anotherClonedModel);
+				await renderer.compileAsync(model, camera, scene);
+			});
+		});
+	}
+	if (target === '6th-floor') {
+		new RGBELoader().setPath(basePath).load('hdr/office.hdr', function (texture) {
+			texture.mapping = THREE.EquirectangularReflectionMapping;
+
+			scene.background = texture;
+			scene.environment = texture;
+			renderer.toneMappingExposure = 10.0;
+			const loader = new GLTFLoader().setPath(basePath);
+			loadModel(loader, 'floors/6thfloor/6thfloor.gltf', [0, 0, 0], true);
+			loadModel(loader, 'floors/6thfloor/6thwalls.gltf', [0, 0, 0]);
+			loadModel(loader, 'floors/6thfloor/6thdoors.gltf', [0, 0, 0]);
+			// loadModel(loader, 'floors/6thfloor/extraStuff.gltf', [0, 0, 0]);
+			loadModel(loader, 'objects/kitchen/kitchen.gltf', [0, 0, 0]);
+
+			// loader.load('objects/tuoli/tuoli.gltf', async function (gltf) {
+			// 	const model = gltf.scene;
+
+			// 	model.position.set(11, 0, -15);
+
+			// 	scene.add(model);
+			// 	const clonedModel = model.clone();
+			// 	clonedModel.position.z += 2.75; // adjust the value as needed
+			// 	scene.add(clonedModel);
+
+			// 	await renderer.compileAsync(model, camera, scene);
+			// });
+			loader.load('objects/smalldesk/smalldesk.gltf', async function (gltf) {
+				const model = gltf.scene;
+				// x-axis (left/right), y-axis (up/down), z-axis (forward/backward)
+
+				model.position.set(10, 0, -10);
+				scene.add(model);
+				const clonedModel = model.clone();
+				// clonedModel.position.z += 2.75; // adjust the value as needed
+				clonedModel.position.set(0, 0, 0);
+				clonedModel.children[0].name = '6th-floor';
 				console.log(clonedModel);
 				interactionGroup.add(clonedModel);
 
