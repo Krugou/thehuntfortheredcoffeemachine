1176d145ae09d07c4ea60058f2b33ec60fc03b67
Krugou <59641229+Krugou@users.noreply.github.com>
2024-04-29 22:08:58 +0300

    Add 5th floor models and textures

--- thegame/src/js/utils/loadmodels.js
+++ thegame/src/js/utils/loadmodels.js
@@ -216,4 +216,61 @@ export function loadmodels(target) {
 			});
 		});
 	}
+	if (target === '5th-floor') {
+		new RGBELoader().setPath(basePath).load('hdr/office.hdr', function (texture) {
+			texture.mapping = THREE.EquirectangularReflectionMapping;
+
+			scene.background = texture;
+			scene.environment = texture;
+			renderer.toneMappingExposure = 10.0;
+			const loader = new GLTFLoader().setPath(basePath);
+			loadModel(loader, 'floors/5thfloor/5thfloor.gltf', [0, 0, 0], true);
+			// loadModel(loader, 'floors/5thfloor/5thwalls.gltf', [0, 0, 0]);
+			// loadModel(loader, 'floors/5thfloor/5thdoors.gltf', [0, 0, 0]);
+			// loadModel(loader, 'floors/5thfloor/extraStuff.gltf', [0, 0, 0]);
+			loadModel(loader, 'objects/kitchen/kitchen.gltf', [0, 0, 0]);
+
+			// loader.load('objects/tuoli/tuoli.gltf', async function (gltf) {
+			// 	const model = gltf.scene;
+
+			// 	model.position.set(11, 0, -15);
+
+			// 	scene.add(model);
+			// 	const clonedModel = model.clone();
+			// 	clonedModel.position.z += 2.75; // adjust the value as needed
+			// 	scene.add(clonedModel);
+
+			// 	await renderer.compileAsync(model, camera, scene);
+			// });
+			// loader.load('objects/smalldesk/smalldesk.gltf', async function (gltf) {
+			// 	const model = gltf.scene;
+			// 	// x-axis (left/right), y-axis (up/down), z-axis (forward/backward)
+
+			// 	model.position.set(10, 0, -10);
+			// 	scene.add(model);
+			// 	const clonedModel = model.clone();
+			// 	// clonedModel.position.z += 2.75; // adjust the value as needed
+			// 	clonedModel.position.set(0, 0, 0);
+			// 	clonedModel.children[0].name = '5th-floor';
+			// 	console.log(clonedModel);
+			// 	interactionGroup.add(clonedModel);
+
+			// 	await renderer.compileAsync(model, camera, scene);
+			// });
+			loader.load('objects/hissi/hissi.gltf', async function (gltf) {
+				const model = gltf.scene;
+
+				model.position.set(0, 0, 0);
+
+				scene.add(model);
+				const clonedModel = model.clone();
+				clonedModel.position.z -= 2.75; // adjust the value as needed
+				scene.add(clonedModel);
+				const anotherClonedModel = clonedModel.clone();
+				anotherClonedModel.position.z -= 2.75; // adjust the value as needed
+				scene.add(anotherClonedModel);
+				await renderer.compileAsync(model, camera, scene);
+			});
+		});
+	}
 }
