82ef949a7ec97e16df1fd4b61fbedded4bb42014
yliysi <catrina.koskinen@gmail.com>
2024-05-02 09:52:45 +0300

    new grip model

--- thegame/src/js/utils/loadGripModel.js
+++ thegame/src/js/utils/loadGripModel.js
@@ -3,15 +3,18 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {basePath} from '../main.js'; // assuming GLTFLoader and THREE are exported from main.js
 export function loadGripModel(controllerGrip1, controllerGrip2) {
 	const loader = new GLTFLoader().setPath(basePath);
-	loader.load('low_poly_blue_handgun_pistol/scene.gltf', function (gltf) {
-		gltf.scene.scale.set(0.1003, 0.1003, 0.1003);
+	//loader.load('low_poly_blue_handgun_pistol/scene.gltf', function (gltf) {
+	loader.load('cup/scene.gltf', function (gltf) {
+		//gltf.scene.scale.set(0.1003, 0.1003, 0.1003);
+		gltf.scene.scale.set(0.05, 0.05, 0.05);
 		let mymodel = gltf.scene;
 		mymodel.name = 'gripModel';
-		mymodel.rotation.y = THREE.MathUtils.degToRad(-90);
-		mymodel.rotation.x = THREE.MathUtils.degToRad(-30);
+		mymodel.rotation.y = THREE.MathUtils.degToRad(-65);
+		mymodel.rotation.x = THREE.MathUtils.degToRad(-80);
+		mymodel.rotation.z = THREE.MathUtils.degToRad(-5);
 
 		// Move the model a little lower
-		mymodel.position.set(0, -0.11, 0);
+		mymodel.position.set(0, 0, 0);
 
 		// Add the model to the first controller
 		controllerGrip1.add(mymodel);
