a01b663bee5032659bf168cce4d6192f3ecd6c62
Krugou <59641229+Krugou@users.noreply.github.com>
2024-05-06 16:54:57 +0300

    refactor: Adjust y-axis position of marker in moveMarker.js

--- thegame/src/js/utils/moveMarker.js
+++ thegame/src/js/utils/moveMarker.js
@@ -34,7 +34,7 @@ export function moveMarker() {
 		if (intersects.length > 0) {
 			INTERSECTION = intersects[0].point;
 			// fix the y-axis to 0
-			INTERSECTION.y = 0.1;
+			INTERSECTION.y = 0.01;
 		}
 	}
 	if (INTERSECTION) marker.position.copy(INTERSECTION);
