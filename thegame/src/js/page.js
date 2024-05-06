9216db2e2d38182c9b468fb477c1369e5d36ad6e
ysi <catrina.koskinen@gmail.com>
2024-05-06 23:24:10 +0300

    add new grip models, styling change for page

--- thegame/src/js/page.js
+++ thegame/src/js/page.js
@@ -21,12 +21,14 @@ const firebaseConfig = {
 	messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
 	appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
+/*
 console.log(import.meta.env.VITE_APP_FIREBASE_API_KEY);
 console.log(import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN);
 console.log(import.meta.env.VITE_APP_FIREBASE_PROJECT_ID);
 console.log(import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET);
 console.log(import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID);
 console.log(import.meta.env.VITE_APP_FIREBASE_APP_ID);
+*/
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export let userDocId;
@@ -38,45 +40,54 @@ body.className =
 
 // Create a container div
 const div = document.createElement('div');
-div.className = 'bg-white rounded-lg shadow-lg p-10 text-center';
+div.className = 'bg-white rounded-lg shadow-lg p-10 text-center border-t-8 border-red-500';
 
 // Create a new element
 const title = document.createElement('h1');
-title.textContent = 'The Hunt for the Red Coffee Machine';
-const instructions = document.createElement('p');
-instructions.textContent =
-	'The goal of the game is to locate the red coffee machine.';
-instructions.className = 'text-base text-red-500 mb-3';
+title.textContent = 'The Hunt For The Red Coffee Machine';
 
-const instructions2 = document.createElement('p');
+const wrapper = document.createElement('div');
+wrapper.className = 'flex flex-col items-center';
+
+const title2 = document.createElement('p');
+title2.textContent = 'How-to-Play:';
+title2.className = 'font-bold text-2xl pb-4 text-left w-3/4';
+const instructions = document.createElement('p');
+instructions.innerHTML =
+	'The goal of the game is to locate the <span class="text-red-500 font-bold">red</span> coffee machine so your cup can be happy. ☕ <br>';
+instructions.className = 'text-base mb-3 text-left w-3/4';
+
+const instructions2 = document.createElement('ul');
 instructions2.innerHTML = `
-<ul class="list-disc list-inside text-red-500 text-base mb-5">
-  <li>Move between floors by using the elevator buttons. Activate the buttons using the trigger button on your controller.</li>
-  <li>Teleport to different locations within the same floor using the squeeze button on your controller.</li>
-</ul>
+  <li class="pb-2">Move between floors by using the elevator buttons. Activate the buttons using the <span class="font-bold">trigger button</span> on your controller.</li>
+  <li>Teleport to different locations within the same floor using the <span class="font-bold">squeeze button</span> on your controller.</li>
 `;
+instructions2.classList='list-disc list-inside text-base text-left mb-5 w-3/4';
+
 // Add Tailwind CSS classes to the title
-title.className = 'text-4xl text-red-500 mb-5';
+title.className = 'text-4xl text-red-500 mb-5 font-bold';
 div.appendChild(title);
-div.appendChild(instructions);
-div.appendChild(instructions2);
+div.appendChild(wrapper);
+wrapper.appendChild(title2);
+wrapper.appendChild(instructions);
+wrapper.appendChild(instructions2);
 
 // Create an input container
 const inputDiv = document.createElement('div');
-inputDiv.className = 'flex justify-center items-center space-x-4';
+inputDiv.className = 'flex justify-center space-x-4 mb-5';
 
 // Create an input field
 const input = document.createElement('input');
 input.type = 'text';
 input.placeholder = 'Enter desired nickname';
 input.className =
-	'border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-red-500 ';
+	'border border-gray-300 rounded-lg p-5 focus:outline-none focus:ring-2 focus:ring-red-500 ';
 
 // Create a button
 const button = document.createElement('button');
 button.textContent = 'Start';
 button.className =
-	'bg-red-500 text-white rounded-lg px-4 py-2 hover:bg-red-800';
+	'bg-red-500 text-white rounded-lg px-4 py-2 hover:bg-red-800 font-bold uppercase';
 
 // Add an event listener to the button
 button.addEventListener('click', startGame);
@@ -90,20 +101,22 @@ input.addEventListener('keypress', function (e) {
 
 inputDiv.appendChild(input);
 inputDiv.appendChild(button);
-div.appendChild(inputDiv);
+wrapper.appendChild(inputDiv);
 const highScoresList = document.createElement('ul');
-highScoresList.className = 'text-center m-2 text-red-500';
-const lifirst = document.createElement('li');
+highScoresList.className = 'text-center m-2 list-decimal';
+const lifirst = document.createElement('p');
 lifirst.textContent = 'High Scores:';
+lifirst.className='text-2xl font-bold pb-2';
 highScoresList.appendChild(lifirst);
 getHighScores(db).then(scores => {
 	scores.forEach(score => {
 		const li = document.createElement('li');
-		li.textContent = `${score.nickName}: ${score.score} seconds`;
+		li.innerHTML = `${score.nickName}: <span class="text-red-500"> ${score.score} seconds</span>`;
+		li.className = 'text-left';
 		highScoresList.appendChild(li);
 	});
 });
-div.appendChild(highScoresList);
+wrapper.appendChild(highScoresList);
 
 body.appendChild(div);
 function startGame() {
--- thegame/src/js/utils/loadGripModel.js
+++ thegame/src/js/utils/loadGripModel.js
@@ -3,23 +3,20 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {basePath} from '../main.js'; // assuming GLTFLoader and THREE are exported from main.js
 export function loadGripModel(controllerGrip1, controllerGrip2) {
 	const loader = new GLTFLoader().setPath(basePath);
-	//loader.load('low_poly_blue_handgun_pistol/scene.gltf', function (gltf) {
-	loader.load('cup/scene.gltf', function (gltf) {
-		//gltf.scene.scale.set(0.1003, 0.1003, 0.1003);
-		gltf.scene.scale.set(0.05, 0.05, 0.05);
+	// right hand flashlight
+	loader.load('flashlight/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(0.02, 0.02, 0.02);
 		let mymodel = gltf.scene;
 		mymodel.name = 'gripModel';
-		mymodel.rotation.y = THREE.MathUtils.degToRad(-65);
-		mymodel.rotation.x = THREE.MathUtils.degToRad(-80);
-		mymodel.rotation.z = THREE.MathUtils.degToRad(-5);
+		mymodel.rotation.y = THREE.MathUtils.degToRad(20);
+		mymodel.rotation.x = THREE.MathUtils.degToRad(-125);
+		mymodel.rotation.z = THREE.MathUtils.degToRad(10);
 
 		// Move the model a little lower
 		mymodel.position.set(0, 0, 0);
 
-		let mymodel2 = mymodel.clone();
 		// Add the model to the first controller
 		controllerGrip1.add(mymodel);
-		// Create a directional light
 		// Create a directional light
 		const light = new THREE.SpotLight(0xffffff, 0.9);
 		light.position.set(0, 0, 0);
@@ -37,12 +34,25 @@ export function loadGripModel(controllerGrip1, controllerGrip2) {
 		// Set the light's target
 		light.target = target;
 
-		controllerGrip2.add(light);
+		controllerGrip1.add(light);
 
 		// Add the target to the controllerGrip2 so it moves with the light
-		controllerGrip2.add(light.target);
+		controllerGrip1.add(light.target);
+	});
+	//left hand red cup
+	loader.load('redcup/scene.gltf', function (gltf) {
+		gltf.scene.scale.set(0.1, 0.1, 0.1);
+		let mymodel = gltf.scene;
+		mymodel.name = 'gripModel';
+		mymodel.rotation.y = THREE.MathUtils.degToRad(80);
+		mymodel.rotation.x = THREE.MathUtils.degToRad(-20);
+		mymodel.rotation.z = THREE.MathUtils.degToRad(-20);
 
-		// Add the cloned model to the second controller
-		controllerGrip2.add(mymodel2);
+		// Move the model a little lower
+		mymodel.position.set(0, 0, 0);
+
+		// Add the model to the first controller
+		controllerGrip2.add(mymodel);
+	
 	});
 }
